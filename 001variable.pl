812d8d68c6a92725ed65c1404e8bfdb22193c23c
danny1023 <1183335185@qq.com>
2019-12-25 15:56:12 +0800

    update1225_002

--- 001variable.pl
+++ 001variable.pl
@@ -8,6 +8,14 @@ print "Age = $age\n";
 print "Name = $name\n";
 print "Salary = $salary\n";
 
+
+$str = "hello" . "world";       # 字符串连接
+$num = 5 + 10;                  # 两数相加
+$mul = 4 * 5;                   # 两数相乘
+$mix = $str . $num;             # 连接字符串和数字
+
+print("$str\n$mix\n");
+
 @ages = (25, 30, 40);             
 @names = ("google", "youj", "taobao");
 
@@ -23,3 +31,42 @@ print "\$names[2] = $names[2]\n";
 print "\$data{'google'} = $data{'google'}\n";
 print "\$data{'youj'} = $data{'youj'}\n";
 print "\$data{'taobao'} = $data{'taobao'}\n";
+
+@names = ('google', 'youj', 'taobao');
+
+@copy = @names;   # 复制数组
+$size = @names;   # 数组赋值给标量，返回数组元素个数
+
+print "名字为 : @copy\n";
+print "名字数为 : $size\n";
+
+
+#多行字符串
+$string = '
+单引号
+	—— 多行字符语法！
+';
+
+print "$string\n";
+
+print <<EOF;
+here
+	—— 多行字符语法！
+EOF
+
+
+print "文件名 ". __FILE__ . "\n";
+print "行号 " . __LINE__ ."\n";
+print "包名 " . __PACKAGE__ ."\n";
+
+# 无法解析
+print "__FILE__ __LINE__ __PACKAGE__\n";
+
+#v字符串
+$smile  = v9786;
+$foo    = v102.111.111;
+$martin = v77.97.114.116.105.110; 
+
+print "smile = $smile\n";
+print "foo = $foo\n";
+print "martin = $martin\n";
